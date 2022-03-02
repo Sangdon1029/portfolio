31c33722cee5bda387fd0ab5a7f0180d62bf5d80
Sangdon1029 <gorist@likelion.org>
2022-03-02 17:15:47 +0900

    포트폴리오 페이지 제작

---
+++ 포트폴리오/style/javascript/main.js
@@ -0,0 +1,4 @@
+const personal = document.getElementById('personalInfo');
+const graduate = document.getElementById('graduateInfo');
+const certificate = document.getElementById('certificateInfo');
+const skill = document.getElementById('skillInfo');
